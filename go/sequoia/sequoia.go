08e007166d67e6efea7641ace6febe30b8a710f0
Daiki Ueno <dueno@redhat.com>
2025-07-18 12:57:47 +0900

    Complete the license change from LGPL-2.0-or-later to Apache-2.0
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- go/sequoia/sequoia.go
+++ go/sequoia/sequoia.go
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0-or-later
+// SPDX-License-Identifier: Apache-2.0
 
 package sequoia
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0-or-later
+// SPDX-License-Identifier: Apache-2.0
 
 mod error;
 pub use crate::error::*;
